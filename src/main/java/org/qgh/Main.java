25d2f26cad7b6311632c4a826395a0a5cf71c485
QuincyGH <415553576@qq.com>
2025-05-20 15:10:41 +0800

    修改.gitignore规则

---
+++ src/main/java/org/qgh/Main.java
@@ -0,0 +1,14 @@
+package org.qgh;
+
+public class Main {
+    public static void main(String[] args) {
+        // 打印开始标记
+        System.out.println("Hello and welcome!");
+        // 循环打印数字1~5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("i = " + i);
+        }
+        // 打印完结标记
+        System.out.println("Print end!");
+    }
+}
